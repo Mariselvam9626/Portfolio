dd59c9b9e5d7717d281cab83c15405796da2e5c7
Mariselvam <167742103+Mariselvam9626@users.noreply.github.com>
2024-04-30 17:04:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Fullstack Developer,","Web Developer","Python developer","Software Engineer"],
+    typeSpeed:100,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true   
+})
